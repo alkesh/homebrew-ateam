27b26e7ba1896ce822f41e7405b95274657ea38f
Alkesh Vaghmaria <alkesh.vaghmaria@smartpension.co.uk>
2021-01-26 17:48:22 +0000

    fix download location

--- Formula/ateam.rb
+++ Formula/ateam.rb
@@ -1,7 +1,7 @@
 class Ateam < Formula
   desc "A tool that helps optimize the code review process"
   homepage "https://github.com/frisoft/ateam"
-  url "https://github.com/alkesh/ateam/archive/0.7.0.tar.gz"
+  url "https://github.com/alkesh/ateam/releases/download/v0.7.0/ateam.tar.gz"
   sha256 "0d91eae42c6abfd2ef0a99c8b25c9730c043c1bc2658e469720d0a25abb5c718"
   version "0.7.0"
 
