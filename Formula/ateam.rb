61b2e0a67c39b7dc75ebdefbbf5e6d252f907467
Alkesh Vaghmaria <alkesh.vaghmaria@smartpension.co.uk>
2021-01-26 17:45:00 +0000

    fix sha

--- Formula/ateam.rb
+++ Formula/ateam.rb
@@ -2,7 +2,7 @@ class Ateam < Formula
   desc "A tool that helps optimize the code review process"
   homepage "https://github.com/frisoft/ateam"
   url "https://github.com/alkesh/ateam/archive/0.7.0.tar.gz"
-  sha256 "0ce5fe46be571cb9ee9c4a5145e95e5abe0478bace1b707ad270b11bca87b437"
+  sha256 "0d91eae42c6abfd2ef0a99c8b25c9730c043c1bc2658e469720d0a25abb5c718"
   version "0.7.0"
 
   def install
